548b41b84cdb35c4906b8a5944bc177b4b66ec21
Fabrice Sznajderman <fabszn@protonmail.com>
2019-11-13 18:11:30 +0100

    clean

--- httpEngine/src/main/scala/org/floxx/repository/redis/Keys.scala
+++ httpEngine/src/main/scala/org/floxx/repository/redis/Keys.scala
@@ -2,7 +2,6 @@ package org.floxx.repository.redis
 
 import java.util.UUID
 
-@deprecated
 object Keys {
 
   sealed trait RedisKey {
@@ -14,20 +13,6 @@ object Keys {
 
   }
 
-  case class SecurityInfo() extends RedisKey {
-    override val _root_key: String   = "security"
-    override val description: String = "contains id for role"
-  }
-
-  case class SchedulesCfp() extends RedisKey {
-
-    override val _root_key: String   = "shedulecfp"
-    override val description: String = "contains all schedules for 3 days (Json format)"
-  }
-
-  case class Hit() extends RedisKey {
-    override val _root_key: String   = "hit"
-    override val description: String = "contains all percentage"
-  }
+  
 
 }
