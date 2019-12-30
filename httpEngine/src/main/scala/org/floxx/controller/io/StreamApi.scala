e3d4761ae971ee1088dfea3f1c843dc7cd477a43
Fabrice Sznajderman <fabszn@protonmail.com>
2019-12-30 16:21:02 +0100

    fix bug ws

--- httpEngine/src/main/scala/org/floxx/controller/io/StreamApi.scala
+++ httpEngine/src/main/scala/org/floxx/controller/io/StreamApi.scala
@@ -2,7 +2,6 @@ package org.floxx.controller.io
 
 import cats.effect.IO
 import cats.effect.IO._
-import cats.effect.concurrent.MVar
 import fs2.concurrent.Queue
 import org.floxx.controller.security.WithSecurity
 import org.floxx.service.SecurityService
@@ -11,9 +10,12 @@ import org.http4s.{ Header, Headers }
 import org.http4s.server.websocket.WebSocketBuilder
 import org.http4s.websocket.WebSocketFrame
 import org.http4s.websocket.WebSocketFrame.Text
+import org.slf4j.{ Logger, LoggerFactory }
 
 @SuppressWarnings(Array("org.wartremover.warts.Any"))
 class StreamApi(ss: SecurityService[IO], channel: Queue[IO, WebSocketFrame]) extends Api with WithSecurity {
+
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   override def api: HandleQuery = {
     case req @ GET -> Root / "api" / "stream" / "hit" => {
@@ -25,9 +27,9 @@ class StreamApi(ss: SecurityService[IO], channel: Queue[IO, WebSocketFrame]) ext
             receive = stream => {
               stream.evalMap {
                 case Text(data, _) => {
-                  channel.enqueue1(Text("send back "))
+                  IO(Unit)
                 }
-                case unknown => IO(println(s"Unknown type: $unknown"))
+                case unknown => IO(logger.error(s"Unknown type: $unknown"))
               }
             },
             Headers(List(Header(`Content-Type`.name.value, "application/json")))
