98ec915de16a712fc70de9d8a57176f5e62879e2
Fabrice Sznajderman <fabszn@protonmail.com>
2024-02-20 08:47:58 +0100

    fix it

--- httpEngine/src/it/scala/fixtures/DataFixtures.scala
+++ httpEngine/src/it/scala/fixtures/DataFixtures.scala
@@ -45,8 +45,8 @@ trait DataFixtures {
   val slots = Seq(
     Slot(
       Slot.Id("wednesday_neu253_t_09:30-12:30_2022"),
-      Room.PkId(321),
-      Room.Id("neu253_t"),
+      Room.Name("neu253_t"),
+      Room.Id(321),
       Slot.FromTime("09:30"),
       Slot.ToTime("12:30"),
       Kind("test1"),
@@ -56,8 +56,8 @@ trait DataFixtures {
     ),
     Slot(
       Slot.Id("wednesday_b_amphi_13:30-16:30_2022"),
-      Room.PkId(765),
-      Room.Id("b_amphi"),
+      Room.Name("b_amphi"),
+      Room.Id(765),
       Slot.FromTime("13:30"),
       Slot.ToTime("16:30"),
       Kind("test2"),
@@ -67,8 +67,8 @@ trait DataFixtures {
     ),
     Slot(
       Slot.Id("wednesday_b_amphi_10:30-11:30_2022"),
-      Room.PkId(987),
-      Room.Id("b_amphi"),
+      Room.Name("b_amphi"),
+      Room.Id(987),
       Slot.FromTime("10:30"),
       Slot.ToTime("11:30"),
       Kind("test3"),
